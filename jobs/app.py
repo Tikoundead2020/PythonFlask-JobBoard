dfaab903987fda1cefd49a280cd2af73bad3a611
Matthew Richardson <matthewrichardson999@gmail.com>
2019-05-25 22:14:46 +0100

    Completed the module

--- jobs/app.py
+++ jobs/app.py
@@ -1,8 +1,8 @@
 from flask import Flask, render_template
 
-app = Flask (__name__)
+app = Flask(__name__)
 
 @app.route('/')
-@app.route('Jobs')
-def Jobs():
+@app.route('jobs')
+def jobs():
     return render_template('index.html')
