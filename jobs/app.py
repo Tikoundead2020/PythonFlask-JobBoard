03d5ba7a6fec8a95a72d4dcb5c1334d603fd4fb3
Matthew Richardson <matthewrichardson999@gmail.com>
2019-05-26 22:22:45 +0100

    Completed the module

--- jobs/app.py
+++ jobs/app.py
@@ -31,7 +31,6 @@ def close_connection(exception):
 @app.route("/")
 @app.route("/Jobs")
 def jobs():
-    jobs = execute_sql('SELECT jobs.id, job.title, job.description,
-    job.salary, employer.id as employer_id, employer.name as employer_name
-    FROM job JOIN ON employer.id = job.employer_id')
+    jobs = execute_sql('SELECT jobs.id, job.title, job.description,job.salary,
+    employer.id as employer_id, employer.name as employer_name from job JOIN ON employer.id = job.employer_id')
     return render_template('index.html', jobs=jobs)
